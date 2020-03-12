ce85fdbd59c8283752fd980cc9503a15b70dcfd2
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2020-03-12 13:18:17 +0100

    Redirect stdout

--- 01-simulate.py
+++ 01-simulate.py
@@ -1,7 +1,7 @@
 import os
-import sys
 import random
-from joblib import Parallel, delayed, parallel_backend
+import joblib
+from stdout_redirector import stdout_redirector
 import ROOT
 
 
@@ -10,12 +10,13 @@ doubleplanes = [8, 12, 30]
 energies = [600]
 erels = [100, 500]
 neutrons = [1, 2, 3, 4, 5]
-filepattern = "output/%dm_%ddp_%dAMeV_%dkeV_%dn.%s.root"
+
 
 ROOT.ROOT.EnableThreadSafety()
 ROOT.FairLogger.GetLogger().SetLogVerbosityLevel("LOW")
 ROOT.FairLogger.GetLogger().SetLogScreenLevel("WARNING")
 
+filepattern = "output/%dm_%ddp_%dAMeV_%dkeV_%dn.%s.root"
 vmcworkdir = os.environ["VMCWORKDIR"]
 os.environ["GEOMPATH"] = vmcworkdir + "/geometry"
 os.environ["CONFIG_DIR"] = vmcworkdir + "/gconfig"
@@ -23,77 +24,84 @@ os.environ["PHYSICSLIST"] = "QGSP_INCLXX_HP"
 
 
 def simulate(distance, energy, doubleplane, neutron, erel, overwrite=False):
-    simufile = filepattern % (distance, doubleplane, energy, erel, neutron, "simu")
+    outfile = filepattern % (distance, doubleplane, energy, erel, neutron, "simu")
 
-    if not overwrite and os.path.isfile(simufile):
-        print("Exists: " + simufile)
-        return
+    # Re-Process only if explicitly asked to
+    if not overwrite and os.path.isfile(outfile):
+        print("Exists: " + outfile)
+        return None
 
-    # Redirect stdout / stderr to log files
-    sys.stdout = open(simufile.replace(".root", ".log"), "w")
-    sys.stderr = open(simufile.replace(".root", ".err"), "w")
+    # Write all output to a log file
+    logfile = outfile.replace(".root", ".log")
+    with stdout_redirector(logfile):
+        # Initialize Simulation
+        run = ROOT.FairRunSim()
+        run.SetName("TGeant4")
+        run.SetStoreTraj(False)
+        run.SetMaterials("media_r3b.geo")
 
-    # Initialize Simulation
-    run = ROOT.FairRunSim()
-    run.SetName("TGeant4")
-    run.SetStoreTraj(False)
-    run.SetMaterials("media_r3b.geo")
+        # Output
+        output = ROOT.FairRootFileSink(outfile)
+        run.SetSink(output)
 
-    # Output
-    output = ROOT.FairRootFileSink(simufile)
-    run.SetSink(output)
+        # Primary Generator
+        generator = ROOT.FairPrimaryGenerator()
+        inputfile = "%s/input/%dSn_%dn_%dAMeV_%dkeV.dat" % (
+            vmcworkdir,
+            132 - neutron,
+            neutron,
+            energy,
+            erel,
+        )
+        generator.AddGenerator(ROOT.R3BAsciiGenerator(inputfile))
+        run.SetGenerator(generator)
 
-    # Primary Generator
-    generator = ROOT.FairPrimaryGenerator()
-    inputfile = "%s/input/%dSn_%dn_%dAMeV_%dkeV.dat" % (
-        vmcworkdir,
-        132 - neutron,
-        neutron,
-        energy,
-        erel,
-    )
-    generator.AddGenerator(ROOT.R3BAsciiGenerator(inputfile))
-    run.SetGenerator(generator)
+        # Geometry
+        cave = ROOT.R3BCave("Cave")
+        cave.SetGeometryFileName("r3b_cave_vacuum.geo")
+        run.AddModule(cave)
 
-    # Geometry
-    cave = ROOT.R3BCave("Cave")
-    cave.SetGeometryFileName("r3b_cave_vacuum.geo")
-    run.AddModule(cave)
+        neuland_position = ROOT.TGeoTranslation(0.0, 0.0, distance + doubleplane * 10.0 / 2.0)
+        neuland = ROOT.R3BNeuland(doubleplane, neuland_position)
+        run.AddModule(neuland)
 
-    neuland_position = ROOT.TGeoTranslation(0.0, 0.0, distance + doubleplane * 10.0 / 2.0)
-    neuland = ROOT.R3BNeuland(doubleplane, neuland_position)
-    run.AddModule(neuland)
+        magnetic_field = ROOT.R3BGladFieldMap("R3BGladMap")
+        magnetic_field.SetScale(-0.6)
+        run.SetField(magnetic_field)
 
-    magnetic_field = ROOT.R3BGladFieldMap("R3BGladMap")
-    magnetic_field.SetScale(-0.6)
-    run.SetField(magnetic_field)
+        # Prepare to run
+        run.Init()
+        ROOT.TVirtualMC.GetMC().SetRandom(ROOT.TRandom3(random.randint(0, 10000)))
+        ROOT.TVirtualMC.GetMC().SetMaxNStep(100000)
 
-    # Prepare to run
-    run.Init()
-    ROOT.TVirtualMC.GetMC().SetRandom(ROOT.TRandom3(random.randint(0, 10000)))
-    ROOT.TVirtualMC.GetMC().SetMaxNStep(100000)
+        # Runtime Database
+        rtdb = run.GetRuntimeDb()
+        parafile = filepattern % (distance, doubleplane, energy, erel, neutron, "para")
+        parout = ROOT.FairParRootFileIo(True)
+        parout.open(parafile)
+        rtdb.setOutput(parout)
+        rtdb.saveOutput()
 
-    # Runtime Database
-    rtdb = run.GetRuntimeDb()
-    parafile = filepattern % (distance, doubleplane, energy, erel, neutron, "para")
-    parout = ROOT.FairParRootFileIo(True)
-    parout.open(parafile)
-    rtdb.setOutput(parout)
-    rtdb.saveOutput()
-
-    run.Run(10000)
-    return
+        run.Run(10000)
+    return None
 
 
+# Single simulation
 # simulate(distance=15, energy=600, doubleplane=8, neutron=1, erel=100, overwrite=True)
 
 
-with parallel_backend('multiprocessing'):
-    results = Parallel(n_jobs=30, verbose=11)(
-        delayed(simulate)(distance, energy, doubleplane, neutron, erel, overwrite=False)
-        for distance in distances
-        for energy in energies
-        for doubleplane in doubleplanes
-        for neutron in neutrons
-        for erel in erels
-    )
+# Parallel simulations
+joblib.Parallel(n_jobs=-1, backend="multiprocessing", verbose=11)(
+    joblib.delayed(simulate)(
+        distance=distance,
+        energy=energy,
+        doubleplane=doubleplane,
+        neutron=neutron,
+        erel=erel,
+        overwrite=False)
+    for distance in distances
+    for energy in energies
+    for doubleplane in doubleplanes
+    for neutron in neutrons
+    for erel in erels
+)
---
+++ stdout_redirector.py
@@ -0,0 +1,60 @@
+# Redirect stdout/stderr from called C/C++ libraries
+# Adapted from
+# https://eli.thegreenplace.net/2015/redirecting-all-kinds-of-stdout-in-python
+
+from contextlib import contextmanager
+import ctypes
+import io
+import os
+import sys
+
+
+@contextmanager
+def stdout_redirector(filename):
+    libc = ctypes.CDLL(None)
+    c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
+    c_stderr = ctypes.c_void_p.in_dll(libc, 'stderr')
+
+    # The original fd stdout points to. Usually 1 on POSIX systems.
+    original_stdout_fd = sys.stdout.fileno()
+    original_stderr_fd = sys.stderr.fileno()
+
+    def _redirect_stdout(to_fd):
+        """Redirect stdout to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stdout)
+        # Flush and close sys.stdout - also closes the file descriptor (fd)
+        sys.stdout.close()
+        # Make original_stdout_fd point to the same file as to_fd
+        os.dup2(to_fd, original_stdout_fd)
+        # Create a new sys.stdout that points to the redirected fd
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+
+    def _redirect_stderr(to_fd):
+        """Redirect stdout to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stderr)
+        # Flush and close sys.stdout - also closes the file descriptor (fd)
+        sys.stderr.close()
+        # Make original_stdout_fd point to the same file as to_fd
+        os.dup2(to_fd, original_stderr_fd)
+        # Create a new sys.stdout that points to the redirected fd
+        sys.stderr = io.TextIOWrapper(os.fdopen(original_stderr_fd, 'wb'))
+
+    # Save a copy of the original stdout fd in saved_stdout_fd
+    saved_stdout_fd = os.dup(original_stdout_fd)
+    saved_stderr_fd = os.dup(original_stderr_fd)
+    try:
+        # Create a log file and redirect stdout to it
+        tfile = open(filename, "w")
+        _redirect_stdout(tfile.fileno())
+        _redirect_stderr(tfile.fileno())
+        # Yield to caller, then redirect stdout back to the saved fd
+        yield
+        _redirect_stdout(saved_stdout_fd)
+        _redirect_stderr(saved_stderr_fd)
+        tfile.flush()
+    finally:
+        tfile.close()
+        os.close(saved_stdout_fd)
+        os.close(saved_stderr_fd)
